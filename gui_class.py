ec53751717fc6a93757cc322166b98639cbabbde
Fatih Satı <fatihsati@gmail.com>
2022-07-23 14:13:01 +0300

    new features added

--- gui_class.py
+++ gui_class.py
@@ -1,78 +1,94 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import *
+import tkinter
+
+from requests import delete
 import zam
+import re
 
-# isimde tarih varsa onu atip yeni tarih koymali regex ile tarihi bul ve at
 # kodu exe dosyasi haline getir
-# github repo acip pushla
-
 
 
 class app:
     
     def __init__(self, master):
-        self.file_path = ''
         frame = Frame(master)
         frame.pack(side='top', fill='x', expand=False)
         
 
-        self.label_text = ''
         self.button = Button(frame, text='Dosya seç', command= self.button_command)
         self.button.pack(side='left')
 
-        self.filename_label = Label(frame, text=self.label_text)    # label style eklenecek
+        self.filename_label = Label(frame, text='')    # label style eklenecek
         self.filename_label.pack(side='left', padx=20, fill='x', expand=True)
 
 
         
-        
-        frame_right = Frame(master)
-        frame_right.pack(side='top', fill='both', expand=True, pady=50)
+        frame_bottom = Frame(master)
+        frame_bottom.pack(side='top', fill='both', expand=True, pady=50)
                 
         
-        self.label1 = Label(frame_right, text='Zam Oranı')
+        self.label1 = Label(frame_bottom, text='Zam Oranı')
         self.label1.grid(row=0, column=0, sticky='w')
 
-        self.zam_orani = Entry(frame_right)
-        self.zam_orani.grid(row=0, column=1, padx=15, sticky='w')
+        self.rise_percent = Entry(frame_bottom)
+        self.rise_percent.grid(row=0, column=1, padx=15, sticky='w')
         
         
-        self.asagi_yuvarla = IntVar()
-        self.cb2 = Checkbutton(frame_right, text= 'Asagi yuvarla', variable=self.asagi_yuvarla).grid(row=1,column=0, sticky='w')
+        self.round_down = IntVar()
+        self.cb2 = Checkbutton(frame_bottom, text= 'Asagi yuvarla', variable=self.round_down).grid(row=1,column=0, sticky='w')
         
-        self.dolar_listesi = IntVar()
-        self.cb3 = Checkbutton(frame_right, text= 'Dolar Listesi', variable=self.dolar_listesi).grid(row=2, column=0, sticky='w')
+        self.is_dollar = IntVar()
+        self.cb3 = Checkbutton(frame_bottom, text= 'Dolar Listesi', variable=self.is_dollar).grid(row=2, column=0, sticky='w')
 
-        self.submit_button = Button(frame_right, text='Zam Yap', command=self.submit_button_func).grid(row=3, column=1, sticky='w', padx=15)
+        self.output_file_name_label = Label(frame_bottom, text='Yeni Dosya Adı').grid(row=3, column=0, sticky='w')
+        self.output_file_name = Entry(frame_bottom)
+        self.output_file_name.grid(row=3, column=1, sticky='w', padx=15, ipadx=50)
+        
+        self.submit_button = Button(frame_bottom, text='Zam Yap', command=self.submit_button_func).grid(row=4, column=1, sticky='w', padx=15, pady=10)
 
-        self.result_label = Label(frame_right, text='')
+        self.result_label = Label(frame_bottom, text='')
         self.result_label.grid(row=5, column=1, ipady=25, sticky='w')
         
         
+        
     def button_command(self):
-        self.file_path = filedialog.askopenfilename()
-        filename = self.file_path.split('/')[-1]
-        self.filename_label["text"] = filename
-    
+        self.file_path = filedialog.askopenfilename()       # get filepath
+        filename = self.file_path.split('/')[-1]            # filename
+        self.path = self.file_path.split(filename)[0]       # path without filenamem - used for saving new file into same location
+        self.filename_label["text"] = filename              # show filename on the top of the screen
+        
+        self.temp_output_file = filename.split('.docx')[0]  # filename without .docx shows on the entry temporarily
+        self.output_file_name.delete(0, 'end')
+        self.output_file_name.insert(0, self.temp_output_file)
     
     def submit_button_func(self):
         
-        if self.asagi_yuvarla.get() == 0:   
+        if self.output_file_name.get() == self.temp_output_file:            # returns an error if output filename is same with the old version
+            messagebox.showinfo(title='Hata', message='Yeni Dosya Adi Eskisi İle Aynı Olamaz!')
+            self.result_label['text'] = 'Dosya Keydedilemedi. Lütfen Tekrar Deneyiniz'
+            return
+        
+        
+        if self.round_down.get() == 0:      # checks if round up or down
             round_up = True
         else:
             round_up = False
         
-        if self.dolar_listesi.get() == 1:
+        if self.is_dollar.get() == 1:   # checks wheter it's dollar list
             para_birimi = '$'
         else:
             para_birimi = '₺'
         
         try:
-            output_file_name = self.file_path.split('.docx')[0] + '23.07.2022.docx'
-            new_doc = zam.change_doc(self.file_path,int(self.zam_orani.get()), round_up, para_birimi)
+            output_file_name = self.output_file_name.get() + '.docx'        # output file name to be saved
+            
+            output_file_name = self.path + output_file_name                 # output file name with full path
+            new_doc = zam.change_doc(self.file_path,int(self.rise_percent.get()), round_up, para_birimi)    # send file into function which will return a new file
 
-            new_doc.save(output_file_name)
+            new_doc.save(output_file_name)      # save new file
             
             self.result_label['text'] = 'Dosya Kaydedildi'
 
--- zam.py
+++ zam.py
@@ -4,27 +4,27 @@ import math
 
 
 
-def format_fiyat(fiyat, para_birimi):
+def price_text_format(price, currency_unit):
 
-    fiyat = str(fiyat) + f'.00{para_birimi}'
-    return fiyat
+    price = str(price) + f'.00{currency_unit}'
+    return price
     
 
-def zam_to_cell(fiyat, zam_orani, round_up, para_birimi):
-    yeni_fiyat = int(fiyat + (fiyat * zam_orani / 100))
+def zam_to_cell(price, zam_orani, round_up, currency_unit):
+    new_price = int(price + (price * zam_orani / 100))
     # en yakin 5'in katina yuvarla
-    if yeni_fiyat % 5 != 0:
+    if new_price % 5 != 0:
         if round_up == True:
-            yeni_fiyat = (math.floor(yeni_fiyat/5)*5)+5
+            new_price = (math.floor(new_price/5)*5)+5
         else:
-            yeni_fiyat = math.floor(yeni_fiyat/5)*5
+            new_price = math.floor(new_price/5)*5
 
-    yeni_fiyat = format_fiyat(yeni_fiyat, para_birimi)
-    return yeni_fiyat
+    new_price = price_text_format(new_price, currency_unit)
+    return new_price
 
 
 
-def change_doc(docname, zam_orani, round_up, para_birimi):
+def change_doc(docname, zam_orani, round_up, currency_unit):
     # docname, zam_orani, round_up = get_inputs()
     doc = Document(docname)
     table = doc.tables[0]
@@ -32,11 +32,11 @@ def change_doc(docname, zam_orani, round_up, para_birimi):
         for cell in row.cells[1:]:
             try:
                 cell_value = cell.text.split('.')[0] 
-                if para_birimi in cell_value:
-                    fiyat = cell_value[1:].strip()
+                if currency_unit in cell_value:
+                    price = cell_value[1:].strip()
                 else:
-                    fiyat = cell_value
-                cell.text = zam_to_cell(int(fiyat), zam_orani, round_up, para_birimi)
+                    price = cell_value
+                cell.text = zam_to_cell(int(price), zam_orani, round_up, currency_unit)
                 
             except:
                 continue
